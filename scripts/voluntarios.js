f042281ee1fe5cdf5e4e9afc6a22e45954da9c51
silashorta <92862835+silashorta@users.noreply.github.com>
2023-04-21 01:08:06 -0300

    # Changes to be committed: #    modified:   index.html #    modified:   scripts/voluntarios.js

--- scripts/voluntarios.js
+++ scripts/voluntarios.js
@@ -11,17 +11,20 @@ function insereDados() {
     const firstRow = table.insertRow();
     firstRow.insertAdjacentHTML('beforeend', '<th>Nome</th>');
     firstRow.insertAdjacentHTML('beforeend', '<th>Categoria</th>');
-    firstRow.insertAdjacentHTML('beforeend', '<th>Visualizar</th>');
-    firstRow.insertAdjacentHTML('beforeend', '<th>Editar</th>');
-    firstRow.insertAdjacentHTML('beforeend', '<th>Excluir</th>');
+    firstRow.insertAdjacentHTML('beforeend', '<th></th>');
+    firstRow.insertAdjacentHTML('beforeend', '<th></th>');
+    firstRow.insertAdjacentHTML('beforeend', '<th></th>');
 
     for (let i = 0; i < integrantes.length; i++) {
         const row = table.insertRow();
+        const divVazia = table.insertRow()
         const nomeCell = row.insertCell();
         const categoriaCell = row.insertCell();
         const botaoModal = row.insertCell()
         const botaoEdit = row.insertCell();
         const botaoExclui = row.insertCell()
+        const modal = row.insertCell()
+
 
         nomeCell.innerHTML = `${integrantes[i].nome} ${integrantes[i].sobrenome}`;
 
@@ -35,10 +38,30 @@ function insereDados() {
             row.style.display = "none";
         }
 
-        botaoEdit.innerHTML = `<button class="btn"><img class="icones__voluntarios" src="../icon/pen-to-square-regular.svg"></button>`
-        botaoExclui.innerHTML = `<button class="btn"><img class="icones__voluntarios" src="../icon/trash-can-regular.svg"></button>`
-        botaoModal.innerHTML = `<button class="btn"><img class="icones__voluntarios" src="../icon/square-minus-regular.svg"></button>`
+        botaoEdit.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="edita" src="../icon/pen-to-square-regular.svg"></button>`
+        botaoExclui.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="exclui" src="../icon/trash-can-regular.svg"></button>`
+        botaoModal.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="visualiza" src="../icon/square-minus-regular.svg"></button>`
+        const visualiza = document.querySelector('#visualiza')
+        botaoModal.addEventListener('click', (e) => {
+            e.preventDefault()
+            visualiza.style.transform = 'rotate(90deg)';   
+            modal.classList.toggle('d-block')
+            console.log(visualiza)   
+        })
 
+        divVazia.appendChild(modal)
+        modal.setAttribute("colspan", "6")
+        modal.style.display = 'none'
+        modal.innerHTML = `
+        <p><strong>#id:</strong> ${integrantes[i].id}</p>
+        <p><strong>Nome:</strong> ${integrantes[i].nome}</p>
+        <p><strong>Sobrenome:</strong> ${integrantes[i].sobrenome}</p>
+        <p><strong>Posicao:</strong> ${integrantes[i].posicao}</p>
+        <p><strong>Categoria:</strong> ${integrantes[i].categoria}</p>
+        <p><strong>Categoria 2:</strong> ${integrantes[i].categoria2}</p>
+        <p><strong>Nascimento:</strong> ${integrantes[i].nascimento}</p>
+        <p><strong>Telefone:</strong> ${integrantes[i].telefone}</p>
+        `
     }
 
 }
