d4910dd00cab3846ade96f240129deb3688a4209
silashorta <92862835+silashorta@users.noreply.github.com>
2023-04-30 03:05:32 -0300

    Update repertório
    link youtube

--- scripts/repertorio.js
+++ scripts/repertorio.js
@@ -9,100 +9,131 @@ function insereDados() {
     table.innerHTML = ""
 
     for (let i = 0; i < cancoes.length; i++) {
+        /*         const row = document.createElement('div')
+                row.className = 'row linha '
+                row.id = 'row'
+                table.appendChild(row)
+        
+                const col1 = document.createElement('div')
+                row.className = 'linha'
+                col1.id = 'col1'
+                row.appendChild(col1)
+        
+                const col2 = document.createElement('div')
+                row.className = 'linha'
+                col2.id = 'col2'
+                row.appendChild(col2)
+        
+                const col3 = document.createElement('div')
+                row.className = 'linha'
+                col3.id = 'col3'
+                row.appendChild(col3)
+        
+                const idCell = document.createElement('div')
+                idCell.className = ''
+                idCell.id = "idCell"
+                col1.appendChild(idCell)
+                idCell.innerHTML = `#<strong>${cancoes[i].id}</strong>`;
+        
+                const tituloCell = document.createElement('div');
+                tituloCell.className = '';
+                tituloCell.id = 'idTitulo';
+                col1.appendChild(tituloCell);
+        
+                if (cancoes[i].titulo === '') {
+                    row.style.display = 'none';
+                } else {
+                    tituloCell.innerHTML = `<strong>${cancoes[i].titulo}</strong> 
+                                        <p class="subtitulo">${cancoes[i].subtitulo}</p>`;
+                }
+        
+        
+                const interpreteCell = document.createElement('div')
+                interpreteCell.className = ''
+                interpreteCell.id = "idInterprete"
+                col1.appendChild(interpreteCell)
+                interpreteCell.innerHTML = `<em>${cancoes[i].interprete}</em>`;
+        
+                const tomFemininoCell = document.createElement('div')
+                tomFemininoCell.className = ''
+                tomFemininoCell.id = "idTomFeminino"
+                col2.appendChild(tomFemininoCell)
+                tomFemininoCell.innerHTML = `<span><strong>Tom Feminino: </strong></span>${cancoes[i].tomFeminino}`;
+        
+                const tomMasculinoCell = document.createElement('div')
+                tomMasculinoCell.className = ''
+                tomMasculinoCell.id = "idTomMasculino"
+                col2.appendChild(tomMasculinoCell)
+                tomMasculinoCell.innerHTML = `<span><strong>Tom Masculino: </strong></span>${cancoes[i].tomMasculino}`;
+        
+                const andamentoCell = document.createElement('div')
+                andamentoCell.className = ''
+                andamentoCell.id = "idAndamento"
+                col2.appendChild(andamentoCell)
+                andamentoCell.innerHTML = `<strong>BPM</strong>: ${cancoes[i].andamento}`;
+        
+                const compassoCell = document.createElement('div')
+                compassoCell.className = ''
+                compassoCell.id = "idCompasso"
+                col2.appendChild(compassoCell)
+                compassoCell.innerHTML = `<strong>Compasso</strong>: ${cancoes[i].compasso}`;
+        
+                const botaoEdit = document.createElement('div')
+                botaoEdit.className = ''
+                botaoEdit.id = "idEdit"
+                col3.appendChild(botaoEdit)
+                botaoEdit.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="edita" src="../icon/square-minus-regular.svg"></button>`
+        
+                const botaoExclui = document.createElement('div')
+                botaoExclui.className = ''
+                botaoExclui.id = "idExclui"
+                col3.appendChild(botaoExclui)
+                botaoExclui.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="exclui" src="../icon/pen-to-square-regular.svg"></button>`
+        
+                const botaoModal = document.createElement('div')
+                botaoModal.className = ''
+                botaoModal.id = "idVisualiza"
+                col3.appendChild(botaoModal)
+                botaoModal.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="visualiza" src="../icon/trash-can-regular.svg"></button>` */
+
         const row = document.createElement('div')
-        row.className = 'row linha '
+        row.className = 'row'
         row.id = 'row'
         table.appendChild(row)
-
-        const col1 = document.createElement('div')
-        row.className = 'linha'
-        col1.id = 'col1'
-        row.appendChild(col1)
-
-        const col2 = document.createElement('div')
-        row.className = 'linha'
-        col2.id = 'col2'
-        row.appendChild(col2)
-
-        const col3 = document.createElement('div')
-        row.className = 'linha'
-        col3.id = 'col3'
-        row.appendChild(col3)
-
-        const idCell = document.createElement('div')
-        idCell.className = ''
-        idCell.id = "idCell"
-        col1.appendChild(idCell)
-        idCell.innerHTML = `#<strong>${cancoes[i].id}</strong>`;
-
-        const tituloCell = document.createElement('div');
-        tituloCell.className = '';
-        tituloCell.id = 'idTitulo';
-        col1.appendChild(tituloCell);
-
-        if (cancoes[i].titulo === '') {
-            row.style.display = 'none';
-        } else {
-            tituloCell.innerHTML = `<strong>${cancoes[i].titulo}</strong> 
-                                <p class="subtitulo">${cancoes[i].subtitulo}</p>`;
-        }
-
-
-        const interpreteCell = document.createElement('div')
-        interpreteCell.className = ''
-        interpreteCell.id = "idInterprete"
-        col1.appendChild(interpreteCell)
-        interpreteCell.innerHTML = `<em>${cancoes[i].interprete}</em>`;
-
-        const tomFemininoCell = document.createElement('div')
-        tomFemininoCell.className = ''
-        tomFemininoCell.id = "idTomFeminino"
-        col2.appendChild(tomFemininoCell)
-        tomFemininoCell.innerHTML = `<span><strong>Tom Feminino: </strong></span>${cancoes[i].tomFeminino}`;
-
-        const tomMasculinoCell = document.createElement('div')
-        tomMasculinoCell.className = ''
-        tomMasculinoCell.id = "idTomMasculino"
-        col2.appendChild(tomMasculinoCell)
-        tomMasculinoCell.innerHTML = `<span><strong>Tom Masculino: </strong></span>${cancoes[i].tomMasculino}`;
-
-        const andamentoCell = document.createElement('div')
-        andamentoCell.className = ''
-        andamentoCell.id = "idAndamento"
-        col2.appendChild(andamentoCell)
-        andamentoCell.innerHTML = `<strong>BPM</strong>: ${cancoes[i].andamento}`;
-
-        const compassoCell = document.createElement('div')
-        compassoCell.className = ''
-        compassoCell.id = "idCompasso"
-        col2.appendChild(compassoCell)
-        compassoCell.innerHTML = `<strong>Compasso</strong>: ${cancoes[i].compasso}`;
-
-        const botaoEdit = document.createElement('div')
-        botaoEdit.className = ''
-        botaoEdit.id = "idEdit"
-        col3.appendChild(botaoEdit)
-        botaoEdit.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="edita" src="../icon/square-minus-regular.svg"></button>`
-
-        const botaoExclui = document.createElement('div')
-        botaoExclui.className = ''
-        botaoExclui.id = "idExclui"
-        col3.appendChild(botaoExclui)
-        botaoExclui.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="exclui" src="../icon/pen-to-square-regular.svg"></button>`
-
-        const botaoModal = document.createElement('div')
-        botaoModal.className = ''
-        botaoModal.id = "idVisualiza"
-        col3.appendChild(botaoModal)
-        botaoModal.innerHTML = `<button class="btn"><img class="icones__voluntarios" id="visualiza" src="../icon/trash-can-regular.svg"></button>`
-
-        const visualiza = document.querySelector('#visualiza')
-        botaoModal.addEventListener('click', (e) => {
-            e.preventDefault()
-            visualiza.style.transform = 'rotate(90deg)';
-            modal.classList.toggle('d-block')
-            console.log(visualiza)
-        })
+        row.innerHTML = `
+        <div class="accordion container" id="accordionExample">
+            <div class="accordion-item ">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${cancoes[i].id}" aria-expanded="false" aria-controls="collapse${cancoes[i].id}">
+                <div><strong id="idTitulo">#${cancoes[i].id} ${cancoes[i].titulo}</strong><div>
+                <div><em>${cancoes[i].subtitulo}</em><div>
+                </button>
+            </h2>
+            <div id="collapse${cancoes[i].id}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body bg-light">
+                    <div class="d-flex">
+                        <div class="col">
+                            <p><em>${cancoes[i].interprete}</em></p>
+                            <div>
+                                <a href="${cancoes[i].linkYoutube}" target="blank"><img class="iconYoutube" src="../icon/youtube.png" alt="ícone Youtube"></img>Link Youtube</a>
+                            </div>
+                            <div>
+                                <img class="iconSpotify" src="../icon/spotify.png"></img>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <p>Tom Feminino: ${cancoes[i].tomFeminino}</p>
+                            <p>Tom Masculino: ${cancoes[i].tomMasculino}</p>
+                            <p>BPM: ${cancoes[i].andamento}</p>
+                            <p>Compasso: ${cancoes[i].compasso}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>4</p>
+                    </div>
+                </div>
+            </div>
+        </div>`
 
     }
 
